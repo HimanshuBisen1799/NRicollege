761900699c2a1540ca8d610261fb3ed3523caa00
Himanshu Bisen <bisenji1799@gmail.com>
2023-11-29 01:22:59 +0530

    hiii

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,19 @@ var mongoose = require('mongoose');
 var passportLocalMongoose = require('passport-local-mongoose');
 
 // mongoose.connect("mongodb://127.0.0.1:27017/rent");
-mongoose.connect(process.env.MONGO_URI)
+// mongoose.connect(process.env.MONGO_URI)
+
+
+async function connectToDatabase() {
+  try {
+    // Your MongoDB connection code here
+    mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+}
+
+connectToDatabase();
 
 
 
