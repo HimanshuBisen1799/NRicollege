4495f5ae63a2b19f41eddcab0971d40763a6cae8
Himanshu Bisen <bisenji1799@gmail.com>
2023-11-29 18:59:38 +0530

    loging

--- routes/index.js
+++ routes/index.js
@@ -27,6 +27,7 @@ const storage = multer.diskStorage({
 
   }
 })
+
 const upload = multer({ storage: storage, fileFilter: fileFilter })
 function fileFilter(req, file, cb) {
   if (file.mimetype === "image/png" || file.mimetype === "image/jpg" || file.mimetype === "image/jpeg") {
