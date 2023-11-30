5c35d70ca236ca136a629c236c9dae3ac2858831
Himanshu Bisen <bisenji1799@gmail.com>
2023-11-30 14:55:25 +0530

    hinasg

--- public/javascripts/nav.js
+++ public/javascripts/nav.js
@@ -7,3 +7,13 @@
 //     });
 // });
 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const mobileMenuButton = document.getElementById("mobile-menu-button");
+    const navMenu = document.querySelector("#nav ul");
+  
+    mobileMenuButton.addEventListener("click", function () {
+      navMenu.classList.toggle("active");
+    });
+  });
+  
